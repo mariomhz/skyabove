dad8dc1a065f8ed28cda46c917abe936823f5750
Mario <mariohrdezdeveloper@gmail.com>
2026-02-13 09:12:18 +0000

    fix: swap plane icon to solid filled style

--- app/page.tsx
+++ app/page.tsx
@@ -65,15 +65,11 @@ export default function Home() {
           width="24"
           height="24"
           viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="absolute bottom-8 w-7 h-7 text-black/30 rotate-90"
+          fill="currentColor"
+          className="absolute bottom-8 w-7 h-7 text-black rotate-90"
           style={{ opacity: 0 }}
         >
-          <path d="M17.8 19.2 16 11l3.5-3.5C21 6 21.5 4 21 3c-1-.5-3 0-4.5 1.5L13 8 4.8 6.2c-.5-.1-.9.1-1.1.5l-.3.5c-.2.5-.1 1 .3 1.3L9 12l-2 3H4l-1 1 3 2 2 3 1-1v-3l3-2 3.5 5.3c.3.4.8.5 1.3.3l.5-.2c.4-.3.6-.7.5-1.2z" />
+          <path d="M21 16v-2l-8-5V3.5A1.5 1.5 0 0 0 11.5 2 1.5 1.5 0 0 0 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z" />
         </svg>
       </section>
       <FlightDashboard />
